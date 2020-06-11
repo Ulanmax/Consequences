654c8c6c97db463dbfc125f20932cebea0482e7b
Maks Niagolov <nyagolovm@gmail.com>
2020-06-11 22:05:35 +0900

    added unit tests

--- ConsequencesTests/ConsequencesTests.swift
+++ ConsequencesTests/ConsequencesTests.swift
@@ -10,25 +10,26 @@ import XCTest
 @testable import Consequences
 
 class ConsequencesTests: XCTestCase {
+    
+    var sut: Model!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        sut = Model()
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
+    
+    func testArraySequencing() {
+        
+        let result = sut.doCalculate(text: "1, 2, 3, 5, 8, 9, 10, 7")
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(result, "1-3, 5, 7-10", "Didn't split the array correctly")
     }
 
 }
